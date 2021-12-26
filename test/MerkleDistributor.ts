a0189c2d3637c56bbc79e82f83e567dd29b03efe
martin0044 <martin0044@gmail.com>
2021-12-26 11:56:56 +0000

    Uniswap Contract Implement
    Replace contract with original Uniswap contracts and adjusted test accordingly

--- test/MerkleDistributor.ts
+++ test/MerkleDistributor.ts
@@ -7,16 +7,19 @@ import keccak256 from "keccak256";
 
 describe("MerkleDistributor", function () {
   const users = [
-    { address: "0xD08c8e6d78a1f64B1796d6DC3137B19665cb6F1F", amount: 10 },
-    { address: "0xb7D15753D3F76e7C892B63db6b4729f700C01298", amount: 15 },
-    { address: "0xf69Ca530Cd4849e3d1329FBEC06787a96a3f9A68", amount: 20 },
-    { address: "0xa8532aAa27E9f7c3a96d754674c99F1E2f824800", amount: 30 },
+    { index: 0, address: "0xD08c8e6d78a1f64B1796d6DC3137B19665cb6F1F", amount: 10 },
+    { index: 1, address: "0xb7D15753D3F76e7C892B63db6b4729f700C01298", amount: 15 },
+    { index: 2, address: "0xf69Ca530Cd4849e3d1329FBEC06787a96a3f9A68", amount: 20 },
+    { index: 3, address: "0xa8532aAa27E9f7c3a96d754674c99F1E2f824800", amount: 30 },
   ];
 
   // equal to MerkleDistributor.sol #keccak256(abi.encodePacked(account, amount));
   const elements = users.map((x) =>
-    utils.solidityKeccak256(["address", "uint256"], [x.address, x.amount])
+    utils.solidityKeccak256(["uint256", "address", "uint256"], [x.index, x.address, x.amount])
   );
+
+  const tokenAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+
 
   it("should claim successfully for valid proof", async () => {
     const merkleTree = new MerkleTree(elements, keccak256, { sort: true });
@@ -26,15 +29,17 @@ describe("MerkleDistributor", function () {
     const leaf = elements[3];
     const proof = merkleTree.getHexProof(leaf);
 
+    console.log(elements)
+
     // Deploy contract
     const Distributor = await ethers.getContractFactory("MerkleDistributor");
-    const distributor = await Distributor.deploy(root);
+    const distributor = await Distributor.deploy(tokenAddress,root);
     await distributor.deployed();
 
     // Attempt to claim and verify success
-    await expect(distributor.claim(users[3].address, users[3].amount, proof))
+    await expect(distributor.claim(users[3].index, users[3].address, users[3].amount, proof))
       .to.emit(distributor, "Claimed")
-      .withArgs(users[3].address, users[3].amount);
+      .withArgs(users[3].index, users[3].address, users[3].amount);
   });
 
   it("should throw for invalid amount or address", async () => {
@@ -47,17 +52,17 @@ describe("MerkleDistributor", function () {
 
     // Deploy contract
     const Distributor = await ethers.getContractFactory("MerkleDistributor");
-    const distributor = await Distributor.deploy(root);
+    const distributor = await Distributor.deploy(tokenAddress,root);
     await distributor.deployed();
 
     // random amount
     await expect(
-      distributor.claim(users[3].address, 10000, proof)
+      distributor.claim(0, users[3].address, 10000, proof)
     ).to.be.revertedWith("MerkleDistributor: Invalid proof.");
 
     // random address
     await expect(
-      distributor.claim(
+      distributor.claim(0,
         "0x94069d197c64D831fdB7C3222Dd512af5339bd2d",
         users[3].amount,
         proof
@@ -72,12 +77,12 @@ describe("MerkleDistributor", function () {
 
     // Deploy contract
     const Distributor = await ethers.getContractFactory("MerkleDistributor");
-    const distributor = await Distributor.deploy(root);
+    const distributor = await Distributor.deploy(tokenAddress,root);
     await distributor.deployed();
 
     // Attempt to claim and verify success
     await expect(
-      distributor.claim(users[3].address, users[3].amount, [])
+      distributor.claim(0, users[3].address, users[3].amount, [])
     ).to.be.revertedWith("MerkleDistributor: Invalid proof.");
   });
 });
